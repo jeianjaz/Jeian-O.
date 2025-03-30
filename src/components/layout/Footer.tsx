7609dd2d6b08404271b34e61f91d8b8901628cc1
jeianjaz <obelidor.jeianjasper@gmail.com>
2025-03-30 15:58:10 +0800

    Simplify footer to 'Made with ❤ by jeianjaz'

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="bg-gray-900 text-white py-6 px-4 text-center">
+      <div className="container mx-auto">
+        <p className="text-gray-400">
+          Made with <span className="text-red-500">❤</span> by jeianjaz.
+        </p>
+      </div>
+    </footer>
+  );
+}
