1066b82c8d1adf2e5cfbe5af30541f975d2f5fa3
jeianjaz <obelidor.jeianjasper@gmail.com>
2025-03-30 16:05:20 +0800

    update

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -4,10 +4,10 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer className="bg-gray-900 text-white py-6 px-4 text-center">
+    <footer className="bg-white text-black py-6 px-4 text-center">
       <div className="container mx-auto">
-        <p className="text-gray-400">
-          Made with <span className="text-red-500">❤</span> by jeianjaz.
+        <p className="text-gray-600">
+          Made with <span className="text-red-500">♡</span> by jeianjaz.
         </p>
       </div>
     </footer>
